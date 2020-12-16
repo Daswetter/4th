6bf08ff71fc0509c6eb5055da18790048db7bf58
Daswetter <67914852+Daswetter@users.noreply.github.com>
2020-12-16 19:19:44 +0300

    Drag'n'Drop

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,13 +1,46 @@
 import './input.scss'
 
-const orientationButton = document.querySelector(".orientation-button")
-const sliderInput = document.querySelector(".input")
+// const orientationButton = document.querySelector(".orientation-button")
+const sliderLine = document.querySelector(".slider__line")
+const sliderRound = document.querySelector(".slider__round")
 const colorButton = document.querySelector(".color-button")
 
-orientationButton.onclick = () => {
-  sliderInput.classList.toggle('add-input');
-}
 colorButton.onclick = () => {
-  console.log('click');
-  sliderInput.classList.toggle('change-color');
+  sliderLine.classList.toggle('slider__line_second-color');
+  sliderRound.classList.toggle('slider__round_second-color');
+}
+
+// orientationButton.onclick = () => {
+//   sliderInput.classList.toggle('add-input');
+// }
+
+
+sliderRound.onmousedown = function(event) {
+  let shiftX = event.clientX - sliderRound.getBoundingClientRect().left;
+
+  sliderRound.style.position = 'absolute';
+  sliderRound.style.zIndex = '100';
+  document.body.append(sliderRound)
+
+  function moveAt(pageX, pageY) {
+    let leftStop = pageX - sliderLine.getBoundingClientRect().left;
+    let rightStop = - pageX + sliderLine.getBoundingClientRect().right;
+
+    leftStop < 0 ? leftStop = 0 : 
+    rightStop < 0 ? rightStop = 0 :
+    sliderRound.style.left = pageX - shiftX + 'px';
+    sliderRound.style.top = sliderLine.getBoundingClientRect().top - sliderLine.getBoundingClientRect().height / 2 + 'px'
+  }
+
+  moveAt(event.pageX, event.pageY);
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  function onMouseUp(event) {
+    document.removeEventListener('mousemove', onMouseMove);
+  }
+  
+
 }
