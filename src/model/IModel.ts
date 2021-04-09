f355f1a1aba17c89a0f60d0135ddb7ccf0264786
Daswetter <daswetter@yandex.ru>
2021-04-09 23:04:58 +0300

    changed IModel

--- src/model/IModel.ts
+++ src/model/IModel.ts
@@ -2,23 +2,14 @@ import '../interface/IOptions'
 
 interface IModel {
   setCurrentValue(part: number): void
-  countCurrentValue(part: number): number
   setCurrentValueForExtra(part: number): void
 
   setCurrentPart(part: number): void
-  countCurrentPart(part: number): number
   setCurrentPartForExtra(part: number): void
 
-  roundToDecimal(value: number, decimal: number): number
-  // countInitialPart(initial: number): number
-  // setInitialPart(): number
-  // setInitialPartForExtra(): number
   countScaleElements(): Array<number>
   bindCurrentChanged(callback: (arg0: number, arg1: number) => void): void
   bindExtraCurrentChanged(callback: (arg0: number, arg1: number) => void): void
-  convertStepSizeToDecimal(stepSize: number) : number
-  // getOptions(): IOptions
-
 }
 
 export { IModel }
